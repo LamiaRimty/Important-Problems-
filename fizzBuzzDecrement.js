5100dd135d6350789eff441960bd3685fb379f71
Lamia Jabin Rimty <lamiajabinrimty@Lamias-MacBook-Air.local>
2022-09-23 11:48:50 +0200

    Fibonacci Generator

---
+++ fizzBuzzDecrement.js
@@ -0,0 +1,30 @@
+var output=[];
+var count=100;
+
+function fizzBuzz(){
+while(  count>=1 ){
+    
+    if(count%3===0 && count%5===0){
+     output.push("FizzBuzz");
+ }
+    
+   else if(count%3===0){
+       output.push("Fizz");
+   } 
+
+ else if(count%5===0){
+     output.push("Buzz");
+ }
+
+    else{
+        output.push(count);
+    }
+    
+    
+}
+    
+     console.log(output);
+     count--;
+}
+
+fizzBuzz();   
